6261c3f001895a31a653e8435986c4a8a731dc41
Abishek V Ashok <abishekvashok@gmail.com>
2021-06-21 17:56:07 +0530

    Adds scripts and Github action for testing Pysa
    Adds python script which compares the issues pysa detects in
    delibrately_vulnerable_app and raise exceptions (return an error) when
    there is a mismatch. The output is also out and compared with diff if
    there is a mismatch.
    
    Adds a github action called pysa that compiles pyre-check from the
    source code in the repository using ocaml, opam, and dune, runs
    setup.sh, and the run_integration_tests.py python script.
    
    Modifies ./scripts/setup.py to build a Makefile without tests.
    
    Significant attention has been given to make sure we cache as many
    dependencies as possible thereby making sure the action can run as
    quickly as possible.
    
    Adds full_results.json that will contain issues known to be true in the
    app. For now, it's just issues from rce.py, but can be expanded later
    on as more vulnerabilities are added to the flask app.
    
    The python script, run_integration_tests.py uses integration_test_utils.py
    to reads issues in resulsts.expected, run pysa, compare issues generated
    by pysa against those specified manually.
    
    run_integration_tests.py and its supporting integration_test_utils.py
    script reside at a new folder: ./tools/integration_tests/.
    
    Modifies .pyre_configuration to exclude checking the folder.
    
    Adds .gitignore containing list of directories generated by running pysa
    and those files generated by running run_integration_tests.py
    
    Modifies setup.sh to tailor to the github actions by creating a
    .pyre_configuration file with taint_models_path pointing to ../../stubs
    and copy the integration test utils to the folder of the
    delibrately_vulnerable_flask_app. setup.sh is referenced during the
    github action as mentioned above.
    
    Adds requirements.txt to contain requirements for the flask app.
    Currently it contains only `flask` but will be expanded later when more
    vulnerabilities are added to flask app.
    
    Signed-off-by: Abishek V Ashok <abishekvashok@gmail.com>

---
+++ tools/integration_tests/run_integration_tests.py
@@ -0,0 +1,43 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+import os
+from pathlib import Path
+
+from integration_test_utils import run_pysa_analysis, LOG
+
+
+def main() -> None:
+    """
+    Entry point function which checks if full_result.json is there, calls
+    functions from integration_test_utils to run pysa, parse
+    full_result.json, and compare the output.
+    """
+    if not os.path.isfile("./full_result.json"):
+        raise FileNotFoundError(
+            "./full_result.json file containing expected issues is not found"
+        )
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+
+    current_directory = Path(__file__).parent.absolute()
+
+    os.chdir(str(current_directory))
+    LOG.info("Running in `%s`", os.getcwd())
+    run_pysa_analysis(
+        current_directory,
+        passthrough_args="",
+        skip_model_verification=True,
+        filter_issues=False,
+    )
+    LOG.info("Run produced expected results")
+
+
+if __name__ == "__main__":
+    main()
