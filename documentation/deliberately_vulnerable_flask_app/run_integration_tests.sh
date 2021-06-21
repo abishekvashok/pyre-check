6e58f36822ad4bf8b8c095e1b8f99e92df505f70
Abishek V Ashok <abishekvashok@gmail.com>
2021-06-21 17:56:07 +0530

    Adds scripts and Github action for testing Pysa
    Adds python script which compares the issues pysa detects in
    delibrately_vulnerable_flask_app and raise exceptions when there is a
    mismatch. The output is also written out and compared with diff in case
    of a mismatch.
    
    Adds a github action called pysa that compiles pyre-check from the
    source code in the repository using ocaml, opam, and dune, runs
    setup.sh, and runs tools/pysa_integration_tests/run.py.
    
    Modifies scripts/setup.py to allow building a Makefile without tests.
    
    Significant attention has been given to make sure we cache as many
    dependencies as possible thereby making sure the action runs as quickly
    as possible.
    
    Adds full_result.json that will contain issues known to be true in the
    app. For now, it is just the 3 issues from rce.py, but can be expanded
    on as more vulnerabilities are added to the flask app.
    
    The python script, run.py uses tools/pysa_integration_tests/utils.py to
    read issues in resulsts.expected, run pysa, compare issues generated by
    pysa against those specified manually, and produce corresponding outputs.
    
    Adds .gitignore containing list of directories generated by running pysa
    and those files generated by running pysa in the
    delibrately_vulnerable_flask app's folder.
    
    Modifies setup.sh to tailor to the github actions by creating a
    .pyre_configuration file with taint_models_path pointing to ../../stubs
    and install requirements of the flask app specified in requirements.txt.
    
    Adds requirements.txt to contain requirements for the flask app.
    Currently it contains only `flask` but will be expanded later when more
    vulnerabilities are added to the flask app that uses various other
    libraries/packages.
    
    Adds a wrapper at
    documentation/deliberately_vulnerable_flask_app/run_integration_tests.py
    that can be used to call tools/pysa_integeration_tests/run.py
    
    Signed-off-by: Abishek V Ashok <abishekvashok@gmail.com>

---
+++ documentation/deliberately_vulnerable_flask_app/run_integration_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+python3 ../../tools/pysa_integration_tests/run.py --skip-model-verification
